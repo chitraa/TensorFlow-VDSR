e5dee7d2cedc9355b4dc963081273180a8d55126
Chitra Singh <chits.26@gmail.com>
2019-03-20 16:19:58 +0100

    adjust image i/p o/p acc to pointpronets; test image path remaining

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import tensorflow as tf
 from  model import VDSR
 flags = tf.app.flags
 FLAGS = flags.FLAGS
+IMAGE_SIZE_POINTPRONET = 48
 flags.DEFINE_integer("epoch", 1500, "Number of epoch")
-flags.DEFINE_integer("image_size", 41, "The size of image input")
-flags.DEFINE_integer("label_size", 41, "The size of image output")
-flags.DEFINE_integer("c_dim", 3, "The size of channel")
+flags.DEFINE_integer("image_size", IMAGE_SIZE_POINTPRONET, "The size of image input")
+flags.DEFINE_integer("label_size", IMAGE_SIZE_POINTPRONET, "The size of image output")
+flags.DEFINE_integer("c_dim", 1, "The size of channel") #grayscale
 flags.DEFINE_boolean("is_train", True, "if the train")
-flags.DEFINE_integer("scale", 3, "the size of scale factor for preprocessing input image")
-flags.DEFINE_integer("stride", 41, "the size of stride") ##because output is 33 * 33
+flags.DEFINE_integer("scale", 1, "the size of scale factor for preprocessing input image")
+flags.DEFINE_integer("stride", IMAGE_SIZE_POINTPRONET, "the size of stride") ##because output is 33 * 33
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Name of checkpoint directory")
 flags.DEFINE_float("learning_rate", 1e-4 , "The learning rate")
 flags.DEFINE_integer("batch_size", 64, "the size of batch")
